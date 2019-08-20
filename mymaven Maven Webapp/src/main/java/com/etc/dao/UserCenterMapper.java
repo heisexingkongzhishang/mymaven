90888df13146a7c7eeb0b9749d70e0a7ec51c682
heisexingkongzhishang <805683046@qq.com>
2019-08-20 16:53:11 +0800

    个人中心代码更改 2019/8/20/16：53

--- mymaven Maven Webapp/src/main/java/com/etc/dao/UserCenterMapper.java
+++ mymaven Maven Webapp/src/main/java/com/etc/dao/UserCenterMapper.java
@@ -1,5 +1,11 @@
 package com.etc.dao;
 
 public interface UserCenterMapper {
+	public int collectionTopic(Integer user_id);
 	
+	public int collectionVideo(Integer user_Id);
+	
+	public int myVideo(Integer user_id);
+	
+	public int myTopic(Integer user_id);
 }
