a44e4d054f703bbaa6203fa54edf9faa670badfd
heisexingkongzhishang <805683046@qq.com>
2019-08-20 15:37:25 +0800

    用户添加属性

--- mymaven Maven Webapp/src/main/java/com/etc/entity/User.java
+++ mymaven Maven Webapp/src/main/java/com/etc/entity/User.java
@@ -9,6 +9,7 @@ public class User {
 	private String user_name; // 用户名称
 	private String user_password; // 用户密码
 	private Integer user_sex; // 性别
+	private String user_picture; //头像图片
 	public Integer getUser_id() {
 		return user_id;
 	}
@@ -39,13 +40,21 @@ public class User {
 	public void setUser_sex(Integer user_sex) {
 		this.user_sex = user_sex;
 	}
-	public User(Integer user_id, String user_phone, String user_name, String user_password, Integer user_sex) {
+	public String getUser_picture() {
+		return user_picture;
+	}
+	public void setUser_picture(String user_picture) {
+		this.user_picture = user_picture;
+	}
+	public User(Integer user_id, String user_phone, String user_name, String user_password, Integer user_sex,
+			String user_picture) {
 		super();
 		this.user_id = user_id;
 		this.user_phone = user_phone;
 		this.user_name = user_name;
 		this.user_password = user_password;
 		this.user_sex = user_sex;
+		this.user_picture = user_picture;
 	}
 	public User() {
 		super();
@@ -53,7 +62,8 @@ public class User {
 	@Override
 	public String toString() {
 		return "User [user_id=" + user_id + ", user_phone=" + user_phone + ", user_name=" + user_name
-				+ ", user_password=" + user_password + ", user_sex=" + user_sex + "]";
+				+ ", user_password=" + user_password + ", user_sex=" + user_sex + ", user_picture=" + user_picture
+				+ "]";
 	}
 	
 
